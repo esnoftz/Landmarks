052dae3cb8d382632b4cb395145c5cc7d72af7ee
esnoftz <esnoftz@gmail.com>
2025-04-04 07:31:47 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by EVANGELINE NOFTZ on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
